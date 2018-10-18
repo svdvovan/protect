d2e6a7564e2e4a6f0ff656f18787bcb4823d865e
SretenskyVD <svdvovan@gmail.com>
2018-10-18 15:25:23 +0300

    добавил прокси

--- src/protect.java
+++ src/protect.java
@@ -15,12 +15,14 @@ import java.util.Iterator;
 
 public class protect {
     public static void main(String[] args) throws IOException {
-        String Hostname = "http://protectorshin.ru/tyres/";
-        String Path = "http://protectorshin.ru/tyres/";
+        String Hostname = "http://protectorshin.ru/discs/";
+        String Path = "http://protectorshin.ru/discs/";
 
+
+String Tovar = "диски";
 
         String CatalogName = "koleso";
-        int LastPage = 1;
+        int LastPage = 33;
         Workbook wb = new HSSFWorkbook();
         CreationHelper createHelper = wb.getCreationHelper();
         Sheet sheet1 = wb.createSheet(CatalogName);
@@ -42,9 +44,16 @@ public class protect {
         for (int count = 1; count <= LastPage; count++) {
             String  Path2 = Path+ "?page=" + Page;
 
+//            System.setProperty("https.proxyHost", "77.242.18.96");
+//            System.setProperty("https.proxyPort", "42949");
+
+
+
+
             //                       Document doc4 = Jsoup.connect(Path2).get()
 
             Document doc1 = Jsoup.connect(Path2).get();
+
 
 
 //            String ID = doc1.getElementsByClass("cart").get().select("input").attr("onclick");
@@ -56,6 +65,9 @@ public class protect {
 
             int yyy = 0;
             for (Element link3 : links3) {
+
+
+
                 System.out.println();
                 String addressUrl3 = (links3.get(yyy).select("a[href]").attr("abs:href"));
                 System.out.println(addressUrl3);
@@ -63,81 +75,81 @@ public class protect {
 
                 String ID = doc1.getElementsByClass("cart").get(yyy).select("input").attr("onclick");
                 System.out.println(ID);
-
-                Document doc4 = Jsoup.connect(addressUrl3).get();
-
-
-
-                String NameProduct = doc4.getElementsByTag("h1").text();
-                System.out.println(NameProduct);
-
-                String MainPrice = doc4.getElementsByClass("price").first().text();
-                System.out.println(MainPrice);
-
+try {
+    Document doc4 = Jsoup.connect(addressUrl3)
+            .proxy("201.174.52.27", 49229)
+         //   .timeout(50000)
+            //.ignoreHttpErrors(true)
+            .ignoreContentType(true)
+            .followRedirects(true)
+            .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.38 Safari/537.36")
+            .get();
 
 
-                String Proizvoditel55 = doc4.getElementsByClass("attribute").text(); //здесь таблица распарсить
+    String NameProduct = doc4.getElementsByTag("h1").text();
+    System.out.println(NameProduct);
 
-                System.out.println(Proizvoditel55);
-
-
-                String SDescr = doc4.getElementsByClass("htabs").next().html();  //всегда ли код первым?
-                System.out.println(SDescr);
+    String MainPrice = doc4.getElementsByClass("price").first().text();
+    System.out.println(MainPrice);
 
 
+    String Proizvoditel55 = doc4.getElementsByClass("attribute").text(); //здесь таблица распарсить
+
+    System.out.println(Proizvoditel55);
 
 
-
-                int rowCount = sheet.getLastRowNum();
-                Row row = sheet.createRow(++rowCount);
-
-                String  pictures = doc4.getElementsByClass("image").select("a").first().attr("abs:href");
-                System.out.println(pictures);
+    String SDescr = doc4.getElementsByClass("htabs").next().html();  //всегда ли код первым?
+    System.out.println(SDescr);
 
 
+    int rowCount = sheet.getLastRowNum();
+    Row row = sheet.createRow(++rowCount);
+
+    String pictures = doc4.getElementsByClass("image").select("a").first().attr("abs:href");
+    System.out.println(pictures);
 
 
+    Cell cell227 = row.createCell(0);
+    cell227.setCellValue(ID);
 
 
-                Cell cell227 = row.createCell(0);
-                cell227.setCellValue(ID);
+    Cell cell1 = row.createCell(1);
+    cell1.setCellValue(NameProduct);
 
 
-                Cell cell1 = row.createCell(1);
-                cell1.setCellValue(NameProduct);
+    Cell cell224 = row.createCell(2);
+    cell224.setCellValue(MainPrice);
+
+    Cell cell2242 = row.createCell(3);
+    cell2242.setCellValue(Tovar);
 
 
-                Cell cell224 = row.createCell(2);
-                cell224.setCellValue(MainPrice);
+    Cell cell425 = row.createCell(24);
+    cell425.setCellValue(pictures);
+
+    Cell cell225 = row.createCell(25);
+    cell225.setCellValue(SDescr);
 
 
-                Cell cell425 = row.createCell(24);
-                cell425.setCellValue(pictures);
-
-                Cell cell225 = row.createCell(25);
-                    cell225.setCellValue(SDescr);
+    Elements table = doc4.getElementsByClass("attribute").select("table");
+    Iterator<Element> ite = table.select("td").iterator();
+    Elements row2 = table.select("td");
 
 
-                Elements table = doc4.getElementsByClass("attribute").select("table");
-                Iterator<Element> ite = table.select("td").iterator();
-                Elements row2 = table.select("td");
+    int y2 = 6;
+    for (Element rows : row2) {
 
 
-                int y2 = 6;
-                for (Element rows : row2) {
+        String Har = ite.next().text();
+        System.out.print(Har);
+        Cell cell10 = row.createCell(y2);
+        cell10.setCellValue(Har);
+        y2++;
+    }
 
 
-                    String Har = ite.next().text();
-                    System.out.print(Har);
-                    Cell cell10 = row.createCell(y2);
-                    cell10.setCellValue(Har);
-                    y2++;
-                }
-
-
-
-
-
+}catch (org.jsoup.HttpStatusException e){
+    e.printStackTrace();}
 
                 System.out.println();
                 yyy++;
